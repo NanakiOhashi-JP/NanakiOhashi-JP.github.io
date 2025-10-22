56dd4785d19044cc24fb1cbc223a9ba3eca9ba5c
Nanaki <nkania.jpn@gmail.com>
2025-10-22 17:18:44 +0900

    refactor: remove NavigationBar component and clean up Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import { NavigationBar } from "@/components/NavBar";
-
 export default function Home() {
   return (
     <div>
-      <NavigationBar />
-
       <h1>NanakiOhashi-JP.github.io</h1>
       <p>
         This is a placeholder page for the NanakiOhashi-JP GitHub Pages site.
         More content will be added soon.
       </p>
       <p>Thank you for visiting!</p>
-
     </div>
   );
 }
